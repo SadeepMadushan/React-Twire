1b3370512ba7d4e6b74ee746d73448c6ab735fe8
SadeepMadushan <yksmadushan123@gmail.com>
2025-01-29 20:14:02 +0800

    Update footer.js

---
+++ my-react/src/footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>© 2025 Twire. All Rights Reserved.</p>
+      <div className="social-icons">
+        <a href="#" target="_blank">Facebook</a>
+        <a href="#" target="_blank">Twitter</a>
+        <a href="#" target="_blank">LinkedIn</a>
+        <a href="#" target="_blank">Instagram</a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
