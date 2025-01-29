23b88f25d00d477a15dcc3992aadb092eadca04c
SadeepMadushan <yksmadushan123@gmail.com>
2025-01-29 20:14:46 +0800

    Update hero.js

---
+++ my-react/src/hero.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <section id="hero" className="hero">
+      <p>Strategic IT solutions & outsourcing, privacy & data protection</p>
+      <h1 className="stacked-heading">
+        <span>Innovations that</span>
+        <span>move your</span>
+        <span>business</span>
+        <span>forward</span>
+      </h1>
+    </section>
+  );
+};
+
+export default Hero;
