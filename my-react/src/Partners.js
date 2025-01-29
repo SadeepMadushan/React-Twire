dffc9203e6355f3e1cac5e3e2b78c835fce8e17d
SadeepMadushan <yksmadushan123@gmail.com>
2025-01-29 20:13:14 +0800

    Update Partners.js

---
+++ my-react/src/Partners.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Partners = () => {
+  return (
+    <section className="logo-grid-container">
+      <h1>Our Trusted Partners</h1>
+      <div className="logo-grid">
+        <img src="https://www.acquest.lk/wp-content/uploads/2021/01/Acquest-logo-01-1-scaled.jpg" alt="Company 1" className="logo" />
+        <img src="https://www.twire.se/wp-content/uploads/2019/06/Svenskl%C3%A5nef%C3%B6rmedlinglogo.png" alt="Company 2" className="logo" />
+        <img src="https://www.twire.se/wp-content/uploads/2019/06/EffectSoft-Logo-e1561097466118.png" alt="Company 3" className="logo" />
+        <img src="https://www.twire.se/wp-content/uploads/2019/06/MinKalenderLogo-e1561097499966.png" alt="Company 4" className="logo" />
+      </div>
+      <div className="logo-row">
+        <img src="https://www.twire.se/wp-content/uploads/2019/06/micropol.png" alt="Company 5" className="logo" />
+        <img src="https://www.twire.se/wp-content/uploads/2019/06/Glitteray.png" alt="Company 6" className="logo" />
+        <img src="https://www.twire.se/wp-content/uploads/2019/06/appspotr.png" alt="Company 7" className="logo" />
+        <img src="https://www.twire.se/wp-content/uploads/2019/06/STORADIO_2_RGB.png" alt="Company 8" className="logo" />
+      </div>
+      <div className="logo-row">
+        <img src="https://www.twire.se/wp-content/uploads/2019/06/esmokes-logo-e1561098728826.png" alt="Company 9" className="logo" />
+        <img src="https://www.twire.se/wp-content/uploads/2019/06/adme_logo_black.png" alt="Company 10" className="logo" />
+        <img src="https://www.twire.se/wp-content/uploads/2019/06/logo-e1561099119307.png" alt="Company 11" className="logo" />
+        <img src="https://www.twire.se/wp-content/uploads/2019/06/themobilestore.png" alt="Company 12" className="logo" />
+      </div>
+    </section>
+  );
+};
+
+export default Partners;
